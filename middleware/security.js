ae9f7424891bd2125f658bb804f906d3d930d70b
ValentinHacker <alternativexxxy@gmail.com>
2018-04-13 16:22:26 +0300

    Add files via upload

--- middleware/security.js
+++ middleware/security.js
@@ -1,22 +1,27 @@
-const fs = module.parent.exports.fs,
-HOME = module.parent.exports.home,
-chalk = module.parent.exports.chalk,
-parent = module.parent.exports,
-url = module.parent.exports.url;
-const STORE = url.parse(module.filename).directory + '/midstore/' + url.parse(module.filename).filename + '.json';
+const parent = module.parent.exports,
+fs = parent.fs,
+chalk = parent.chalk,
+PUBLIC = parent.home + '/public',
+PRIVATE = parent.home + '/private',
+url = parent.url,
+BUILTIN = parent.home + '/builtin',
+HOME = parent.home,
+rl = parent.rl,
+STORE = url.parse(module.filename).directory + '/midstore/' + url.parse(module.filename).filename + '.json';
 
 exports.after = [];
-exports.before = ['command', 'fix', 'directory', 'static', 'end'];
+exports.before = ['socket', 'command', 'fix', 'directory', 'static', 'end'];
 exports.name = 'security';
+exports.alive = true;
+
 var store = exports.store = {
-	enabled: true,
 	cooldown: 500
 };
 
 try {
 	fs.ensureFileSync(STORE);
-	exports.store = JSON.parse(fs.readFileSync(STORE));
-} catch (err) {
+	store = exports.store = JSON.parse(fs.readFileSync(STORE));
+} catch(err) {
 	fs.writeFile(STORE, JSON.stringify(exports.store || '{}'), err => {
 		if (!err) console.info(chalk`{green ${module.filename} Initialized.}`);
 	});
@@ -25,10 +30,10 @@ try {
 var reqarr = exports.reqarr = [], reqlist = {};
 
 exports.middleware = function middleware(req, res, msg) {
-	if (reqlist[req.socket.remoteAddress] && req.method == 'POST') {
+	if (reqlist[req.socket.remoteAddress] && (req.method == 'POST' || (req.method == 'GET' && !req.headers.referer))) {
 		let err = new Error('Cooldown...');
 		err.code = 'ECOOL';
-		req.satisfied.error = err;
+		msg.satisfied.error = err;
 		req.emit('err', err);
 		req.destroy(err);
 	} else if (req.method == 'POST') {
@@ -38,9 +43,9 @@ exports.middleware = function middleware(req, res, msg) {
 		req.on('close', () => {
 			reqarr.rmv(req);
 		});
-		req.pass(res, msg);
+		msg.pass();
 	} else {
-		req.pass(res, msg);
+		msg.pass();
 	}
 	return req.satisfied;
 };
