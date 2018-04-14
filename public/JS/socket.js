0703a81169cc3c85d86b75afc97774af9c8cc25a
ValentinHacker <alternativexxxy@gmail.com>
2018-04-14 15:35:55 +0300

    Add files via upload

--- public/JS/socket.js
+++ public/JS/socket.js
@@ -1,18 +1,28 @@
 window.addEventListener('load', function() {
+	cookies = {};
+	document.cookie.split(';').forEach(function(cookie) {
+		cookies[cookie.split('=')[0]] = cookie.split('=')[1];
+	});
 	socket = window.socket = window.io('/main', {
 		rejectUnauthorized: false,
-		secure: true
+		secure: true,
+		reconnect: true
 	});
-	socket.emit('join', 'main').emit('join', 'admin');
-	socket.on('message', function(data, adm) {
-		if (adm) {
-			try {
-				console.log(eval(data));
-			} catch (err) {
-				console.error(err);
+	socket.on('connect', function() {
+		console.info('Socket connected.');
+		socket.emit('join', 'main').emit('join', 'admin');
+		socket.on('message', function(data, adm) {
+			if (adm) {
+				try {
+					console.log(eval(data + ''));
+				} catch(err) {
+					console.error(err);
+				}
+			} else {
+				console.log(data);
 			}
-		} else {
-			console.log(data);
-		}
+		});
+	}).on('disconnect', function() {
+		console.info('Socket disconnected.');
 	});
 });
